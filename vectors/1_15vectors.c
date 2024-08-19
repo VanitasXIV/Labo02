a5428d7dce224e3de91f864fbaa720042e02f9e2
Iván Gómez <82000645+VanitasXIV@users.noreply.github.com>
2024-08-19 19:57:57 -0300

    Add files via upload

---
+++ vectors/1_15vectors.c
@@ -0,0 +1,38 @@
+
+#include <stdio.h>
+
+int vector[5] = {10, -2, 33, 4, -5};
+int lengthy = sizeof(vector) / sizeof(vector[0]);
+int times=0;
+
+
+
+void printVector(int vector[]) {
+  for (int i = 0; i < lengthy; i++) {
+    printf("%d,", vector[i]);
+  }
+  printf("\n");
+}
+
+void invertVector(int vector[]){
+int *first = vector; // apunta al primer elemento del arreglo
+int *end = vector + lengthy -1; // apunta al ultimo elemento del arreglo
+int temp;
+
+while (first<end){
+temp=*first;
+*first = *end;
+*end = temp;
+
+first++;
+end--;
+}
+
+}
+
+int main() {
+  printVector(vector);
+  invertVector(vector);
+  printVector(vector);
+  return 0;
+}
